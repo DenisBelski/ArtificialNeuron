e9096ea1a941db3e8ed19a20c23b3cc0b57933ea
LXG <belsky89@gmail.com>
2022-07-20 18:49:05 +0300

    Added the ability to enter data from the keyboard and convert to multiple units of measurement.

---
+++ Neuron.cs
@@ -0,0 +1,28 @@
+﻿namespace ArtificialNeuron
+{
+    public class Neuron
+    {
+        private decimal weight = 0.5m;
+        public decimal LastError { get; private set; }
+        public decimal Smoothing { get; set; } = 0.000001m;
+
+        public decimal ProcessInputData(decimal input)
+        {
+            return weight * input;
+        }
+
+        public decimal RestoreInputData(decimal output)
+        {
+            return output / weight;
+        }
+
+        public void Train(decimal input, decimal expectedResult)
+        {
+            var actualResult = input * weight;
+            LastError = expectedResult - actualResult;
+            var correction = (LastError / actualResult) * Smoothing;
+            weight += correction;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,52 +4,112 @@ namespace ArtificialNeuron
 {
     class Program
     {
-        public class Neuron
-        {
-            private decimal weight = 0.5m;
-            public decimal LastError { get; private set; }
-            public decimal Smoothing { get; set; } = 0.00001m;
-
-            public decimal ProcessInputData(decimal input)
-            {
-                return weight * input;
-            }
-
-            public decimal RestoreInputData(decimal output)
-            {
-                return output / weight;
-            }
-
-            public void Train(decimal input, decimal expectedResult)
-            {
-                var actualResult = input * weight;
-                LastError = expectedResult - actualResult;
-                var correction = (LastError / actualResult) * Smoothing;
-                weight += correction;
-            }
-        }
+        private const decimal Kilometers = 1;
 
         static void Main(string[] args)
         {
-            decimal kilometers = 100;
-            decimal miles = 62.1371m;
+            bool endApp = false;
+            Console.WriteLine("Console App 'Convert km using artificial neuron'");
+            Console.WriteLine("-----------------------------------------------");
 
+            while (!endApp)
+            {
+                Console.Write("Enter distance in km, and then press Enter: ");
+                string numInput = Console.ReadLine();
+
+                decimal sourceKm;
+                while (!decimal.TryParse(numInput, out sourceKm))
+                {
+                    Console.Write("This is not valid input. Please enter a decimal value: ");
+                    numInput = Console.ReadLine();
+                }
+
+                Console.WriteLine("Choose a conversion option from the following list:" +
+                    "\n\tm - Convert to miles" +
+                    "\n\tv - Convert to versts" +
+                    "\n\tn - Convert to nautical miles" +
+                    "\nYour option? ");
+
+                numInput = Console.ReadLine().ToLower();
+
+                while (numInput != "m" && numInput != "v" && numInput != "n")
+                {
+                    Console.Write("This is not valid input. Сhoose one of the options 'm', 'v' or 'n': ");
+                    numInput = Console.ReadLine();
+                }
+
+                if (numInput == "m")
+                {
+                    ConvertToMiles(sourceKm, Kilometers);
+                }
+                else if (numInput == "v")
+                {
+                    ConvertToVersts(sourceKm, Kilometers);
+                }
+                else if (numInput == "n")
+                {
+                    ConvertToNauticalMiles(sourceKm, Kilometers);
+                }
+
+                Console.WriteLine("-----------------------------------------------\n");
+                Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+
+                if (Console.ReadLine().ToLower() == "n")
+                {
+                    endApp = true;
+                }
+
+                Console.WriteLine("\n");
+            }
+        }
+
+        private static void ConvertToNauticalMiles(decimal sourceKm, decimal Kilometers)
+        {
+            decimal nauticalMiles = 0.5395m;
             Neuron neuron = new Neuron();
+            Console.WriteLine("\nArtificial learning has begun. Please wait...\n");
 
             int i = 0;
             do
             {
                 i++;
-                neuron.Train(kilometers, miles);
-                //Console.WriteLine($"Iterations {i}\tError:\t{neuron.LastError}");
-            } 
+                neuron.Train(Kilometers, nauticalMiles);
+            }
+            while (neuron.LastError > neuron.Smoothing || neuron.LastError < -neuron.Smoothing);
+            Console.WriteLine($"Learning completed successfully!\nYour result is {neuron.ProcessInputData(sourceKm):0.00} nautical miles in {sourceKm} kilometers");
+        }
+
+        private static void ConvertToVersts(decimal sourceKm, decimal Kilometers)
+        {
+            decimal versts = 0.9373m;
+            Neuron neuron = new Neuron();
+            Console.WriteLine("\nArtificial learning has begun. Please wait...\n");
+
+            int i = 0;
+            do
+            {
+                i++;
+                neuron.Train(Kilometers, versts);
+            }
+            while (neuron.LastError > neuron.Smoothing || neuron.LastError < -neuron.Smoothing);
+            Console.WriteLine($"Learning completed successfully!\nYour result is {neuron.ProcessInputData(sourceKm):0.00} versts in {sourceKm} kilometers");
+        }
+
+        private static void ConvertToMiles(decimal sourceKm, decimal Kilometers)
+        {
+            decimal miles = 0.6213m;
+            Neuron neuron = new Neuron();
+            Console.WriteLine("\nArtificial learning has begun. Please wait...\n");
+
+            int i = 0;
+            do
+            {
+                i++;
+                neuron.Train(Kilometers, miles);
+            }
             while (neuron.LastError > neuron.Smoothing || neuron.LastError < -neuron.Smoothing);
 
-            Console.WriteLine("Learning completed");
-
-            Console.WriteLine($"{neuron.ProcessInputData(100)} miles in {100} kilometers");
-            Console.WriteLine($"{neuron.ProcessInputData(541)} miles in {541} kilometers");
-            Console.WriteLine($"{neuron.RestoreInputData(10)} kilometers in {10} miles");
+            Console.WriteLine($"Learning completed successfully!\nYour result is {neuron.ProcessInputData(sourceKm):0.00} miles in {sourceKm} kilometers");
         }
     }
 }
